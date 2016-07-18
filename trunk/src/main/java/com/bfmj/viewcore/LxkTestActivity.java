cb4e5bb99fa9149315064f0cf793c3a5cebb45d8
houguoli <houguoli@8f0ceb4d-bb2a-48bc-9ca4-d49d2b3afe13>
2016-07-18 03:18:16 +0000

    修改测试代码
    git-svn-id: http://192.168.12.55/svn/VRROM/MJViewCore@126 8f0ceb4d-bb2a-48bc-9ca4-d49d2b3afe13

--- trunk/src/main/java/com/bfmj/viewcore/LxkTestActivity.java
+++ trunk/src/main/java/com/bfmj/viewcore/LxkTestActivity.java
@@ -2,11 +2,18 @@ package com.bfmj.viewcore;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import com.bfmj.viewcore.interfaces.IGLPlayerListener;
+import com.bfmj.viewcore.player.GLSystemPlayer;
+import com.bfmj.viewcore.view.GLPlayerView;
 import com.bfmj.viewcore.view.GLRootView;
 import com.bfmj.viewcore.view.GLSenceView;
 
 public class LxkTestActivity extends BaseViewActivity {
 	private GLRootView rootView;
+
+	GLSystemPlayer player;
+
 
 	@SuppressLint("SdCardPath")
 	@Override
@@ -15,13 +22,70 @@ public class LxkTestActivity extends BaseViewActivity {
 		super.onCreate(savedInstanceState);
 		
 		rootView = getRootView();
-		rootView.setDoubleScreen(false);
+		//rootView.setDoubleScreen(false);
 		rootView.onResume();
 
+//		player = new GLSystemPlayer(this);
+//		player.setVideoPath("/mnt/sdcard/111/1.mp4");
+//		player.setLayoutParams(0, 0, 960, 960);
+////		player.set3D(true);
+//		//player.rotate(90);
+//		player.setListener(new IGLPlayerListener() {
+//
+//			@Override
+//			public void onVideoSizeChanged(GLPlayerView player, int width, int height) {
+//				// TODO Auto-generated method stub
+//
+//			}
+//
+//			@Override
+//			public void onTimedText(GLPlayerView player, String text) {
+//				// TODO Auto-generated method stub
+//
+//			}
+//
+//			@Override
+//			public void onSeekComplete(GLPlayerView player) {
+//				// TODO Auto-generated method stub
+//
+//			}
+//
+//			@Override
+//			public void onPrepared(GLPlayerView player) {
+//				player.start();
+//			}
+//
+//			@Override
+//			public boolean onInfo(GLPlayerView player, int what, Object extra) {
+//				// TODO Auto-generated method stub
+//				return false;
+//			}
+//
+//			@Override
+//			public boolean onError(GLPlayerView player, int what, int extra) {
+//				// TODO Auto-generated method stub
+//				return false;
+//			}
+//
+//			@Override
+//			public void onCompletion(GLPlayerView player) {
+//				// TODO Auto-generated method stub
+//
+//			}
+//
+//			@Override
+//			public void onBufferingUpdate(GLPlayerView player, int percent) {
+//				// TODO Auto-generated method stub
+//
+//			}
+//		});
+//
+//		rootView.addView(player);
+
 		GLSenceView senceView = new GLSenceView(this);
-		//senceView.setImage(R.drawable.bg);
+		senceView.setImage(R.drawable.sence);
 		senceView.setObjFile("qiu.obj");
-		senceView.scale(0.5f);
+		senceView.scale(4.0f);
 		senceView.rotate(180, 0, 0, 1);
 		senceView.rotate(180, 0, 1, 0);
 		rootView.addView(senceView);
