64dcb3a2b9a73dc30e73068b0156cc838c7698c9
lixianke <lixianke@8f0ceb4d-bb2a-48bc-9ca4-d49d2b3afe13>
2016-09-18 06:16:15 +0000

    自动打包
    git-svn-id: http://192.168.12.55/svn/VRROM/MJViewCore@1929 8f0ceb4d-bb2a-48bc-9ca4-d49d2b3afe13

--- trunk/buildjar.bat
+++ trunk/buildjar.bat
@@ -7,4 +7,7 @@ xcopy viewcore\build\intermediates\classes\release output\ /s
 xcopy viewcore\build\intermediates\bundles\release\assets output\assets\ /s
 del /f/s/q output\com\bfmj\viewcore\R*.*
 cd output
-jar cf MJViewCore.jar .
+jar cf ..\MJViewCore.jar .
+mkdir arm
+cd ..
+xcopy viewcore\build\intermediates\bundles\release\jni\armeabi output\arm\ /s
